843b78a211cfb443f9a8cb1560250e8a631797e0
Gustavo F. Uessler <gustavouessler@gmail.com>
2023-06-28 14:42:39 -0300

    website under cosntruction2

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,23 @@
 // lib/prisma.ts
 import { PrismaClient } from '@prisma/client';
 
-let prisma: PrismaClient;
-
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
+declare global {
+  var prisma: PrismaClient | undefined
 }
 
-export default prisma;
+export const prisma =
+  global.prisma ||
+  new PrismaClient()
+
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma
+
+// if (process.env.NODE_ENV === 'production') {
+//   prisma = new PrismaClient();
+// } else {
+//   // if (!global.prisma) {
+//   //   global.prisma = new PrismaClient();
+//   // }
+//   prisma = global.prisma;
+// }
+
+// export default prisma;
